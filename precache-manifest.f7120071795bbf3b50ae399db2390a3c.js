e79da56896ba71750d8f4ebacad61dd88b66fb29
SHIVAM MISHRA <shivammishra2628@gmail.com>
2022-09-12 19:35:55 +0530

    Updates

---
+++ precache-manifest.f7120071795bbf3b50ae399db2390a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da38c135871c4dc10f5",
+    "url": "/Upshots/static/css/main.d3c06774.chunk.css"
+  },
+  {
+    "revision": "4da38c135871c4dc10f5",
+    "url": "/Upshots/static/js/main.a87fca2c.chunk.js"
+  },
+  {
+    "revision": "352778a9ad0232d43245",
+    "url": "/Upshots/static/js/runtime~main.c4bcfa49.js"
+  },
+  {
+    "revision": "4c94d86acd64495f1afa",
+    "url": "/Upshots/static/js/2.229e2796.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/Upshots/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "cacea2f4aa41d500f389de01345730f8",
+    "url": "/Upshots/index.html"
+  }
+];
